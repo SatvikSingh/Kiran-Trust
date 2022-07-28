b3fcea691fe6f341bbcf5f86434ea9c35d9d52d2
SatvikSingh <64217296+SatvikSingh@users.noreply.github.com>
2022-07-29 00:41:53 +0530

    frontend updated

--- frontend/src/components/DoctorSection/DoctorSection.js
+++ frontend/src/components/DoctorSection/DoctorSection.js
@@ -1,87 +1,55 @@
 import React from "react";
 import "./DoctorSection.css";
+import Doctor from "./Doctor.js";
+
+const doctors = [
+  {
+    doctorname: "Dr. Abc Xyz1",
+    doctorspl: "XYZ",
+    doctor_desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nemo harum repellendus aut itaque. Temporibus quaerat dolores ut, cupiditate molestiae commodi! Distinctio praesentium, debitis aut minima doloribus earum quia commodi.",
+    doctor_img:"https://images.unsplash.com/photo-1495603889488-42d1d66e5523?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=130&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=130",
+  },
+  {
+    doctorname: "Dr. Abc Xyz2",
+    doctorspl: "XYZ",
+    doctor_desc:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod.",
+    doctor_img:"https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    doctorname: "Dr. Abc Xyz2",
+    doctorspl: "XYZ",
+    doctor_desc:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod.",
+    doctor_img:"https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    doctorname: "Dr. Abc Xyz2",
+    doctorspl: "XYZ",
+    doctor_desc:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod.",
+    doctor_img:"https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    doctorname: "Dr. Abc Xyz2",
+    doctorspl: "XYZ",
+    doctor_desc:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod.",
+    doctor_img:"https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+];
+
 const DoctorSection = () => {
   return (
     <div>
-      <figure class="snip1336">
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample87.jpg"
-          alt="sample87"
-        />
-        <figcaption>
-          <img
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample4.jpg"
-            alt="profile-sample4"
-            class="profile"
-          />
-          <h2>
-            Hans Down<span>Engineer</span>
-          </h2>
-          <p>
-            I'm looking for something that can deliver a 50-pound payload of
-            snow on a small feminine target. Can you suggest something?
-            Hello...?{" "}
-          </p>
-          <a href="#" class="follow">
-            Follow
-          </a>
-          <a href="#" class="info">
-            More Info
-          </a>
-        </figcaption>
-      </figure>
-      <figure class="snip1336 hover">
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample74.jpg"
-          alt="sample74"
-        />
-        <figcaption>
-          <img
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample2.jpg"
-            alt="profile-sample2"
-            class="profile"
-          />
-          <h2>
-            Wisteria Widget<span>Photographer</span>
-          </h2>
-          <p>
-            Calvin: I'm a genius, but I'm a misunderstood genius. Hobbes: What's
-            misunderstood about you? Calvin: Nobody thinks I'm a genius.
-          </p>
-          <a href="#" class="follow">
-            Follow
-          </a>
-          <a href="#" class="info">
-            More Info
-          </a>
-        </figcaption>
-      </figure>
-      <figure class="snip1336">
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample69.jpg"
-          alt="sample69"
-        />
-        <figcaption>
-          <img
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample5.jpg"
-            alt="profile-sample5"
-            class="profile"
-          />
-          <h2>
-            Desmond Eagle<span>Accountant</span>
-          </h2>
-          <p>
-            If you want to stay dad you've got to polish your image. I think the
-            image we need to create for you is "repentant but learning".
-          </p>
-          <a href="#" class="follow">
-            Follow
-          </a>
-          <a href="#" class="info">
-            More Info
-          </a>
-        </figcaption>
-      </figure>
+      <div className="doctorheading text-center">Doctors</div>
+      <div className="doctor-section">
+          <section id="cards">
+            <div className="container py-2">
+              <div className="row">
+                {doctors.map(function (item) {
+                  return <Doctor item={item} key={item.doctorname} />;
+                })}
+              </div>
+            </div>
+          </section>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Layout/Header/Header.js
+++ frontend/src/pages/Layout/Header/Header.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../../assets/company-logo.png";
-// import {Helmet} from "react-helmet";
+import script from "./HeaderScript.js";
 const Header = () => {
   return (
     <section className="navigation">
-      {/* <Helmet>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"/>
-        <script src="./HeaderScript.js" />
-      </Helmet> */}
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"/>
+      <script src={script} type="text/javascript" />
       <div className="nav-container">
         <div className="brand">
           <img id="company-logo" src={logo} alt="Company Logo" />
@@ -21,22 +19,30 @@ const Header = () => {
           </div>
           <ul className="nav-list">
             <li>
-              <a  className="link hover-2" href="#!">Home</a>
+              <a className="link hover-2" href="#!">
+                Home
+              </a>
             </li>
             <li>
-              <a  className="link hover-2" href="#!">About Us</a>
+              <a className="link hover-2" href="#!">
+                About Us
+              </a>
             </li>
             <li>
-              <a  className="link hover-2" href="#!">Doctors</a>
+              <a className="link hover-2" href="#!">
+                Doctors
+              </a>
             </li>
             <li>
-              <a  className="link hover-2" href="#!">Contact Us</a>
+              <a className="link hover-2" href="#!">
+                Contact Us
+              </a>
             </li>
           </ul>
         </nav>
-      </div>      
+      </div>
     </section>
   );
-};  
+};
 
 export default Header;
