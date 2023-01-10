ef994f0e8682f238ab1a803791db3034eae928b0
Raghav Agarwal <agarwalraghav892@gmail.com>
2023-01-10 22:38:04 +0530

    UI changes

--- frontend/src/components/DoctorSection/DoctorSection.js
+++ frontend/src/components/DoctorSection/DoctorSection.js
@@ -1,68 +1,34 @@
 import React from "react";
 import "./DoctorSection.css";
+import Doctorcard from "../DoctorCard/Doctorcard";
 const DoctorSection = () => {
   return (
     <div>
       <section className="section4">
         <div className="title_header">
           <h1>Our Doctors</h1>
-        </div>
+        </div>     
         <div className="row">
           <div className="column">
-            <img
-              src="https://i.ibb.co/Sft7gKp/bg2.webp"
-              className="hover-shadow cursor"
-              alt="doctorimg"
-            />
+            <Doctorcard/>
           </div>
           <div className="column">
-            <img
-              src="https://i.ibb.co/Sft7gKp/bg2.webp"
-              className="hover-shadow cursor"
-              alt="doctorimg"
-            />
+            <Doctorcard/>
           </div>
           <div className="column">
-            <img
-              src="https://i.ibb.co/Sft7gKp/bg2.webp"
-              className="hover-shadow cursor"
-              alt="doctorimg"
-            />
+            <Doctorcard/>
           </div>
           <div className="column">
-            <img
-              src="https://i.ibb.co/Sft7gKp/bg2.webp"
-              className="hover-shadow cursor"
-              alt="doctorimg"
-            />
+            <Doctorcard/>
           </div>
           <div className="column">
-            <img
-              src="https://i.ibb.co/Sft7gKp/bg2.webp"
-              className="hover-shadow cursor"
-              alt="doctorimg"
-            />
+            <Doctorcard/>
           </div>
           <div className="column">
-            <img
-              src="https://i.ibb.co/Sft7gKp/bg2.webp"
-              className="hover-shadow cursor"
-              alt="doctorimg"
-            />
+            <Doctorcard/>
           </div>
           <div className="column">
-            <img
-              src="https://i.ibb.co/Sft7gKp/bg2.webp"
-              className="hover-shadow cursor"
-              alt="doctorimg"
-            />
-          </div>
-          <div className="column">
-            <img
-              src="https://i.ibb.co/Sft7gKp/bg2.webp"
-              className="hover-shadow cursor"
-              alt="doctorimg"
-            />
+            <Doctorcard/>
           </div>
         </div>
       </section>
